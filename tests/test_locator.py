aaf93c34746a3955a05dced7fffb11aee8fe50b8
ssnyta <94050114+ssnyta@users.noreply.github.com>
2025-04-03 13:09:51 +0200

    funguje

--- tests/test_locator.py
+++ tests/test_locator.py
@@ -1,9 +1,16 @@
-from playwright.sync_api import sync_playwright
+import os
+from playwright.sync_api import sync_playwright,expect
+import urllib.parse
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
-    page = browser.new_page()
-    page.goto("file://" + "index.html")  # Načte lokální HTML soubor
-    print(page.title())  # Vypíše titulek stránky
-    browser.close()
-    nadpis_2 = page.locator("text= 'Nadpis 2'")
+def test_page_title():
+    file_path = os.path.abspath("../index.html")
+    file_path = urllib.parse.unquote(file_path)
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(f"file:///{file_path}")
+        nadpis_1 = page.locator('h1').first
+        expect(nadpis_1).to_be_visible()
+        #title = page.title()print(title)
+        browser.close()
